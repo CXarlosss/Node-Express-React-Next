99ed565d93aa2c0a3378f88ea0400956f6bb3d81
Carlos <carlosdepet@gmail.com>
2025-07-29 16:18:44 +0200

    feat: actualizar las categorías en la ruta de categorías

--- DEVTREE/backend/src/routes/categorias.ts
+++ DEVTREE/backend/src/routes/categorias.ts
@@ -1,13 +1,13 @@
-// src/routes/categorias.ts
+// routes/categorias.ts
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', (_req, res) => {
   res.json([
-    { id: 1, nombre: 'Frontend' },
-    { id: 2, nombre: 'Backend' },
-    { id: 3, nombre: 'Fullstack' }
+    { id: 1, nombre: 'JavaScript' },
+    { id: 2, nombre: 'React' },
+    { id: 3, nombre: 'Node.js' },
   ]);
 });
 
